a1737e59772c005bd58e696d10d539aafa29dea9
ccollazzo <151088045+ccollazzo@users.noreply.github.com>
2024-02-28 19:36:17 +0000

    main

--- PhonebookEntry.java
+++ PhonebookEntry.java
@@ -10,7 +10,26 @@ package template;
  */
 
 class PhonebookEntry{
+	long phoneNumber;
+	String type;
 
- 
+	public PhonebookEntry(long phoneNumber, String type) {
+		this.phoneNumber = phoneNumber;
+		this.type = type;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
-	
